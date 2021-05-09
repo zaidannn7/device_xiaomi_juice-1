119fcb0370d776b6ba15add3849e9355486f426b
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-05-09 09:22:23 +0800

    juice: add stock bluetooth props

--- props.mk
+++ props.mk
@@ -1,3 +1,13 @@
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplussho=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
+    vendor.bluetooth.soc=cherokee
+
 # IMS
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
