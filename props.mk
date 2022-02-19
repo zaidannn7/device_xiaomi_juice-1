0bfba6b2af393fab5446d405901e89aaccb3a543
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-02-19 21:06:10 +0530

    juice: Force enable debug.sf.latch_unsignaled
    Update props.mk

--- props.mk
+++ props.mk
@@ -32,6 +32,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
+    debug.sf.latch_unsignaled=0 \
     vendor.display.disable_rotator_downscale=1 \
     debug.renderengine.backend=skiaglthreaded \
     debug.hwui.renderer=skiagl \
