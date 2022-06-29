796a04b83653d8aa2a6b9270e85767d170461eaf
Avinash Dwivedi <kalkioss10@gmail.com>
2022-06-29 16:46:14 +0530

    juice: Add props from CAF
    * From: https://git.codelinaro.org/clo/la/platform/vendor/qcom/bengal/-/blob/617edcfa13e4a525ec3fd9cd9c3816573b14818e/system.prop
    
    They were present in MiUI's Android 11 vendor.prop but for some reason moved to system.prop in 12. So we'll keep the ones we need, here for now.

--- props.mk
+++ props.mk
@@ -15,6 +15,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
+# Bringup properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    config.disable_rtt=true \
+    dev.pm.dyn_samplingrate=1 \
+    persist.sys.force_sw_gles=1 \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.radio.atfwd.start=true \
+    qcom.hw.aac.encoder=true \
+    qemu.hw.mainkeys=0 \
+    ro.kernel.qemu.gles=0
+
 # Cached app compaction
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.device_config.activity_manager.use_compaction=true
@@ -30,6 +41,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.bionic.2nd_cpu_variant=cortex-a73 \
     dalvik.vm.isa.arm64.variant=cortex-a73 \
     dalvik.vm.isa.arm.variant=cortex-a73
+
+# Data modules
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.qti.va_aosp.support=1 \
+    persist.data.netmgrd.qos.enable=true \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
 
 # Debug
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -47,6 +65,31 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # DRM
 PRODUCT_PRODUCT_PROPERTIES += \
     drm.service.enabled=true
+
+# Enable dsp gapless mode by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    audio.offload.gapless.enabled=true
+
+# Enable offload audio video playback by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    av.offload.enable=true
+
+# Enable perf boosts from System Server
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.perf.scroll_opt=1 \
+    vendor.perf.gestureflingboost.enable=true
+
+# Enable stm events
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.debug.coresight.config=stm-events
+
+# Enable tunnel encoding for amrwb
+PRODUCT_PRODUCT_PROPERTIES += \
+    tunnel.audio.encode = true
+
+# Enable voice path for PCM VoIP by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    use.voice.path.for.pcm.voip=true
 
 # FUSE Passthrough
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -73,11 +116,29 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.low_ram=false \
     ro.lmk.log_stats=false
 
+# HWUI properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.texture_cache_size=72
+
 # IMS
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# Initialize QCA1530 detection
+PRODUCT_PRODUCT_PROPERTIES += \
+    sys.qca1530=detect
 
 # IORap
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -95,9 +156,27 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     vendor.audio.offload.buffer.size.kb=256
 
+# MM modules
+PRODUCT_PRODUCT_PROPERTIES += \
+    media.aac_51_output_enabled=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-scan=true \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    mmp.enable.3g2=true \
+    persist.mm.enable.prefetch=true \
+    vendor.mm.enable.qcom_parser=16777215
+
 # Netflix custom property
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.netflix.bsp_rev=Q6115-31409-1
+
+# NFC DT
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.nfc.port=I2C \
 
 # OEM Unlock
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -114,6 +193,31 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # RCS
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.rcs.supported=1
+
+# RIL
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.rild.nitz_long_ons_0= \
+    persist.rild.nitz_long_ons_1= \
+    persist.rild.nitz_long_ons_2= \
+    persist.rild.nitz_long_ons_3= \
+    persist.rild.nitz_plmn= \
+    persist.rild.nitz_short_ons_0= \
+    persist.rild.nitz_short_ons_1= \
+    persist.rild.nitz_short_ons_2= \
+    persist.rild.nitz_short_ons_3= \
+    ril.subscription.types=NV,RUIM \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
+
+# RmNet Data
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.ul_mode=5 \
+    persist.data.wda.enable=true \
+    persist.rmnet.data.enable=true
 
 # Surfaceflinger Offsets
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -132,6 +236,14 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.media.transcoding.codec_max_operating_rate_720P=60 \
     debug.media.transcoding.codec_max_operating_rate_1080P=60
+
+# Turn on CdmaLTEPhone always
+PRODUCT_PRODUCT_PROPERTIES += \
+    telephony.lteOnCdmaDevice=1
+
+# Time-services
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.timed.enable=true
 
 # WFD
 PRODUCT_PRODUCT_PROPERTIES += \
