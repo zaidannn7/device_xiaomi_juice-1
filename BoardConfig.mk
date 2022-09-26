1e1ac1113d316c113ab596f74d8563baa301ace5
zaidannn7 <zaidanmovic0512@gmail.com>
2022-09-26 21:30:08 +0700

    juice : Silence clang warnings during kernel compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
