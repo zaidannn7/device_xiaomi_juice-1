9499cfb06365167c1b5b77edfce500bff9cd7430
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-13 08:31:28 +0700

    juice: KrakenProject Bringup
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_juice.mk
+    $(LOCAL_DIR)/aosp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_juice-user \
-    voltage_juice-userdebug \
-    voltage_juice-eng
+    aosp_juice-user \
+    aosp_juice-userdebug \
+    aosp_juice-eng
--- aosp_juice.mk
+++ aosp_juice.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common voltage stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := voltage_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Juice
@@ -23,9 +23,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Resolution of Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
 
 # Fingerprint
 BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys
-
-# Voltage stuff
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
