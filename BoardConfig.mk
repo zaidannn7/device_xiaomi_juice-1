866df47fff9192c9dad008b8e8a7ba9def3d5133
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-26 05:26:37 +0700

    juice: fix derp
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,7 +110,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 # Tollchain
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_CROSS_COMPILE := aarch64-linux-gnu
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
 
